67e6aff228934941155e784079401c96d29fcb09
iamdroppy <habboclub97@gmail.com>
2024-01-26 16:31:48 +0100

    Initial

---
+++ SportRadar.WorldCupFootball/Class1.cs
@@ -0,0 +1,26 @@
+﻿namespace SportRadar.WorldCupFootball;
+
+public interface ICountry
+{
+    string Name { get; }
+    string Code { get; }
+    float StrengthMultiplier { get; }
+}
+
+public interface IMatch
+{
+    ICountry Home { get; }
+    ICountry Away { get; }
+    int HomeScore { get; }
+    int AwayScore { get; }
+    
+    void HomeGoal();
+    void AwayGoal();
+}
+
+public interface IGroup
+{
+    string Name { get; }
+    IGroup AddCountry(ICountry country);
+    IEnumerable<IMatch> Mix();
+}
